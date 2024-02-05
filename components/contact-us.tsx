0a3df1902ff599f7f9d7e637e183a420c1a8d43e
Harsh Shekhawat <harshshekhawat@Harshs-MacBook-Air.local>
2024-02-05 23:21:16 +0530

    deployment error fixes

--- components/contact-us.tsx
+++ components/contact-us.tsx
@@ -126,7 +126,7 @@ const contactus = () => {
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12038.94967373808!2d72.62475240351267!3d23.19101348435253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1700586000775!5m2!1sen!2sin"
               width="500"
               height="450"
-              allowfullscreen="allowfullscreen"
+              allowFullScreen="allowfullscreen"
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
@@ -138,7 +138,7 @@ const contactus = () => {
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12038.94967373808!2d72.62475240351267!3d23.19101348435253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1700586000775!5m2!1sen!2sin"
               width="500"
               height="450"
-              allowfullscreen="allowfullscreen"
+              allowFullScreen="allowfullscreen"
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
